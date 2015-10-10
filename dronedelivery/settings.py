9e9f263b63d8be5143f3fd1164aabb6349aa2513
drebain <admin@rebainservices.ca>
2015-10-09 21:46:46 -0400

    Temporarily change to sqlite

--- dronedelivery/settings.py
+++ dronedelivery/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'dronedelivery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dronedelivery',
-        'USER': 'admin',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
