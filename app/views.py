0790d9fadd3d97504496fd650297fbb6bb480613
Curtis Smith <curtissmith@Curtiss-MacBook-Pro-2.local>
2015-10-09 17:06:09 -0700

    removed some bad print lines, added a .gitignore

--- app/views.py
+++ app/views.py
@@ -7,6 +7,4 @@ from app.models import InvoiceItem, Invoice
 def index(request):
 	pending_invoice_items = InvoiceItem.objects.filter(invoice__status='pending')
 	context = {'pending_invoice_items': pending_invoice_items}
-	print 'wtf..'
-	print pending_invoice_items
 	return render(request, 'app/index.html', context)
